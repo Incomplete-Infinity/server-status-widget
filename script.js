ff001d9e2e6f31227e215d4983bf316c7b114a00
PhobiaCide <aamason86@gmail.com>
2024-03-26 16:18:26 -0500

    * feat(.gitignore): add .history directory to gitignore * feat(.vscode/settings.json): add cSpell.words configuration for Fragma * feat(index.html): update Halfmoon CSS and Topcoat CSS links, update server status section layout and styles * feat(script.js): add helper functions for logging, calculating uptime, and pausing execution, update server status element layout and styles, add automatic server status update every 30 seconds * feat(style.css): update font-family and styles for body, headings, server status section, and table

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const { log } = console;
 const now = () => new Date();
+const pause = (callback, milliseconds) => setTimeout(callback, milliseconds);
+
+// Helper function to calculate uptime
+const calculateUptime = (startTime, endTime) => {
+  const diff = endTime - startTime;
+  const seconds = Math.max(Math.floor(diff / 1000), 0);
+  const minutes = Math.max(Math.floor(seconds / 60), 0);
+  const hours = Math.max(Math.floor(minutes / 60), 0);
+
+  return seconds + minutes + hours > 0
+    ? `<span class="text-warning fs-5">
+         <span style="width:2em;" class="text-success fs-3">${
+        hours % 24
+      }</span>h : <span style="width:2em;" class="text-success fs-3">${
+        minutes % 60
+      }</span>m : <span style="width:2em;" class="text-success fs-3">${
+        seconds % 60
+      }</span>s</span>`
+    : `0`;
+};
+
+
 // Define the custom element
 class EveServerStatus extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = "Loading server status...";
+    this.innerHTML = `<h3 class="text-center align-center pt-3"><span class="spinner-border"></span>&nbsp;Loading Server Status...</h3>`;
     this.fetchServerStatus();
   }
 
@@ -12,43 +34,49 @@ class EveServerStatus extends HTMLElement {
       "https://esi.evetech.net/latest/status/?datasource=tranquility";
 
     fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const { server_version, players, start_time } = data;
         const isOnline = players > 0;
-        const serverStatus = (isOnline) ? `Online 😀` : `Offline 💔`;
+        const serverStatus = isOnline
+          ? `<span class="text-success">Online</span>😀`
+          : `<span class="text-danger">Offline</span>💔`;
 
+        const playerCount = players
+          ? `<span class="text-info">${players}</span>`
+          : `<span class="text-warning">${0}</span>`;
         const startTime = new Date(start_time);
         const uptime = calculateUptime(startTime, now());
 
         this.innerHTML = `
-        <table>
-        <thead>
-          <th id="fw-header">
-            <h3>Faction Warfare</h3>
-          </th>
-        </thead>
-      </table>
-      <hr>
-      <table>
-        <tr>
-          <td class="column-a">⚡&nbsp;Server&nbsp;Status&nbsp;:&nbsp;</td>
-          <td class="column-b ${isOnline ? 'online' : 'offline'}" id="status">${serverStatus}</td>
-        </tr>
-        <tr>
-          <td class="column-a">👨‍👩‍👧‍👦&nbsp;Player&nbsp;Count&nbsp;:&nbsp;</td>
-          <td class="column-b" id="player-count">${players ?? 0}</td>
-        </tr>
-        <tr>
-          <td class="column-a">⏱&nbsp;Uptime&nbsp;:&nbsp;</td>
-          <td class="column-b" id="uptime"><span id="uptime">${uptime}</span></td>
-        </tr>
-        <tr>
-          <td class="column-a">🎰&nbsp;Version&nbsp;:&nbsp;</td>
-          <td class="column-b" id="version">${server_version ?? 'n/a'}</td>
-        </tr>
-      </table>
-            `;
+<table class="table table-borderless table-stripped table-striped-columns table-hover">
+  <thead>
+    <tr>
+      <th colspan="2" id="fw-header">
+        <h2>Eve Online Server Status</h2>
+        <h4 class="text-secondary">SINGULARITY</h4>
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="text-end">⚡&nbsp;Server&nbsp;Status&nbsp;:&nbsp;</td>
+      <td id="status" class="text-start">${serverStatus}</td>
+    </tr>
+    <tr>
+      <td class="column-a">👨‍👩‍👧‍👦&nbsp;Player&nbsp;Count&nbsp;:&nbsp;</td>
+      <td class="column-b" id="player-count">${playerCount}</td>
+    </tr>
+    <tr>
+      <td class="column-a">⏱&nbsp;Uptime&nbsp;:&nbsp;</td>
+      <td class="column-b" id="uptime"><span id="uptime">${uptime}</span></td>
+    </tr>
+    <tr>
+      <td class="column-a">🎰&nbsp;Version&nbsp;:&nbsp;</td>
+      <td class="column-b" id="version">${server_version ?? "n/a"}</td>
+    </tr>
+  </tbody>
+</table>`;
 
         this.updateUptime(startTime);
       })
@@ -62,25 +90,20 @@ class EveServerStatus extends HTMLElement {
 
     setInterval(() => {
       const uptime = calculateUptime(startTime, now());
-      uptimeElement.textContent = uptime;
+      uptimeElement.innerHTML = uptime; // Use innerHTML instead of textContent
     }, 1000);
   }
 }
 
-// Define the custom element tag
-customElements.define("eve-server-status", EveServerStatus);
+const init = () => {
+  // Define the custom element tag
+  customElements.define("eve-server-status", EveServerStatus);
 
-// Helper function to calculate uptime
-function calculateUptime(startTime, endTime) {
-  const diff = endTime - startTime;
-  const seconds = Math.max(Math.floor(diff / 1000), 0);
-  const minutes = Math.max(Math.floor(seconds / 60), 0);
-  const hours = Math.max(Math.floor(minutes / 60), 0);
+  // Automatically update the server status every 60 seconds
+  setInterval(() => {
+    const serverStatusElement = document.querySelector("eve-server-status");
+    serverStatusElement.fetchServerStatus();
+  }, 30000);
+};
 
-  return (seconds + minutes + hours > 0) ? `${hours % 24}h : ${minutes % 60}m : ${seconds % 60}s` : `0`;
-}
-// Automatically update the server status every 60 seconds
-setInterval(() => {
-  const serverStatusElement = document.querySelector("eve-server-status");
-  serverStatusElement.fetchServerStatus();
-}, 45000);
+pause(init, 1000);
